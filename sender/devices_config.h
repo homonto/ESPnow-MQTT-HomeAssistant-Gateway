4e09fefb365a896789fb842ad8f7505370eb3448
Zygfryd Homonto <46562447+homonto@users.noreply.github.com>
2022-06-19 22:49:49 +0100

    version: 1.8.1 - initial on github

--- sender/devices_config.h
+++ sender/devices_config.h
@@ -1,5 +1,4 @@
 #pragma once
-
 /*
 device configuration template:
 
--- sender/passwords.h
+++ sender/passwords.h
@@ -1,3 +1,7 @@
+/*
+example
+*/
+
 #define BT_SSID               "ssid"
 #define BT_PASSWORD           "password"
 //only path - WITHOUT the binary file name i.e. http://my.server.com/espbinary/devicex"
